44e4a2985f5afda4265238a891c751b9c8c3fba6
Carlos Martinez <carlosmart626@gmail.com>
2016-10-10 23:01:44 -0500

    update django init command

--- umusicfy/run_web.sh
+++ umusicfy/run_web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python manage.py migrate --settings=umusicfy.settings.prd
-python manage.py collectstatic --settings=umusicfy.settings.prd
+python manage.py collectstatic --no-input --settings=umusicfy.settings.prd
